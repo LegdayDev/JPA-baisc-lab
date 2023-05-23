b24c6eac5b09343ba1ef76a84055dac568c4ce41
JaeYoung <chlwodud0327@naver.com>
2023-05-23 18:46:35 +0900

    구현 클래스마다 테이블 생성 전략( 비추 )

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,9 +3,9 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @DiscriminatorColumn //설명하는 컬럼
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue
     private Long id;
